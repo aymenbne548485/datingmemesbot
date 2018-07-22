f9fa58c986e75565cd971f6042b718342e6cb6dc
labrador936 <40704323+labrador936@users.noreply.github.com>
2018-07-22 17:01:09 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,54 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '&'
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Eψ(r) = −ℏ^2/2m ∇^2ψ(r)+V(r)ψ(r)`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+const randomPuppy = require('random-puppy');
+client.on('message', msg => { 
+     
+        if (msg.content.startsWith(prefix + `meme`)) {
+           let args = msg.content.split(" ").slice(1);
+randomPuppy('memes')
+    .then(url => {
+        const embed = new Discord.RichEmbed()
+            .setTimestamp()
+            .setImage(url)
+            .setColor('RANDOM')
+        msg.channel.send(embed);
+    });
+}
+});
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN); 
